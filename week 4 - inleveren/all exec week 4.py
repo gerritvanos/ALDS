060c3297bb027faec7c1953b9950d5e67f45ff3a
gerritvanos <gerrit.vanos@student.hu.nl>
2018-12-13 20:18:57 +0100

    edited feedback week 4

--- week 4 - inleveren/all exec week 4.py
+++ week 4 - inleveren/all exec week 4.py
@@ -104,8 +104,8 @@ def test_seperat_chain_hash_table():
 def find_and_print_fraction_with_same_hash():
     hashes_dict = dict()
     while True:
-        new_number = random.randint(1,2**32)/3**22;
-        new_number_hash = hash(new_number)
+        new_number = random.random()
+        new_number_hash = hash(new_number)%(2**24)
         if new_number_hash in hashes_dict:
             print("found same hash for new_number: ",repr(new_number))
             print("the number in the dict is: ",hashes_dict[new_number_hash])
